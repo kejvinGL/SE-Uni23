8121733bdab0f79e6a04f615d91af2c3503c801b
kejvinGL <kejvinbraka@gmail.com>
2023-08-05 12:45:43 +0200

    port

--- server.js
+++ server.js
@@ -155,6 +155,6 @@ app.post("/applyInternship", async (req, res) => {
     });
   });
 })
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
